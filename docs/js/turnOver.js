0da47a77c3bde5b0dab7fa11d3b96d7834ade808
disaq1 <slava79198015981@yandex.ru>
2021-04-17 13:10:42 +0400

    Other

---
+++ docs/js/turnOver.js
@@ -0,0 +1,70 @@
+var ci=document.querySelector(".catalog__item");
+var qwe=document.querySelector(".description__more--link")
+var qweq=document.querySelector(".qweq")
+
+
+var id1 = document.getElementById('1');
+
+function testId1() {
+    id1.classList.toggle('final');
+    if (id1.classList.contains('final')) {
+        qweq.innerText = 'Назад';
+    } else {
+        qwe.innerText = 'Подробнее о тарифе...'
+    }
+}
+
+var id2 = document.getElementById('2');
+
+function testId2() {
+    id2.classList.toggle('final');
+    if (id2.classList.contains('final')) {
+        qweq.innerText = 'Назад';
+    } else {
+        qwe.innerText = 'Подробнее о тарифе...'
+    }
+}
+
+var id3 = document.getElementById('3');
+
+function testId3() {
+    id3.classList.toggle('final');
+    if (id3.classList.contains('final')) {
+        qweq.innerText = 'Назад';
+    } else {
+        qwe.innerText = 'Подробнее о тарифе...'
+    }
+}
+
+var id4 = document.getElementById('4');
+
+function testId4() {
+    id4.classList.toggle('final');
+    if (id4.classList.contains('final')) {
+        qweq.innerText = 'Назад';
+    } else {
+        qwe.innerText = 'Подробнее о тарифе...'
+    }
+}
+
+var id5 = document.getElementById('5');
+
+function testId5() {
+    id5.classList.toggle('final');
+    if (id5.classList.contains('final')) {
+        qweq.innerText = 'Назад';
+    } else {
+        qwe.innerText = 'Подробнее о тарифе...'
+    }
+}
+
+var id6 = document.getElementById('6');
+
+function testId6() {
+    id6.classList.toggle('final');
+    if (id6.classList.contains('final')) {
+        qweq.innerText = 'Назад';
+    } else {
+        qwe.innerText = 'Подробнее о тарифе...'
+    }
+}
