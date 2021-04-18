b60b197c6dbff2acbbe2aeb4cf9df9802f26f899
disaq1 <slava79198015981@yandex.ru>
2021-04-18 18:28:13 +0400

    Убирает лишние строчки в функции

--- #src/js/turnOver.js
+++ #src/js/turnOver.js
@@ -1,70 +1,75 @@
-﻿var ci=document.querySelector(".catalog__item");
-var qwe=document.querySelector(".description__more--link")
-var qweq=document.querySelector(".qweq")
-
-
-var id1 = document.getElementById('1');
-
+﻿var id1 = document.getElementById('1');
 function testId1() {
     id1.classList.toggle('final');
-    if (id1.classList.contains('final')) {
-        qweq.innerText = 'Назад';
-    } else {
-        qwe.innerText = 'Подробнее о тарифе...'
-    }
 }
 
 var id2 = document.getElementById('2');
-
 function testId2() {
     id2.classList.toggle('final');
-    if (id2.classList.contains('final')) {
-        qweq.innerText = 'Назад';
-    } else {
-        qwe.innerText = 'Подробнее о тарифе...'
-    }
 }
 
 var id3 = document.getElementById('3');
-
 function testId3() {
     id3.classList.toggle('final');
-    if (id3.classList.contains('final')) {
-        qweq.innerText = 'Назад';
-    } else {
-        qwe.innerText = 'Подробнее о тарифе...'
-    }
 }
 
 var id4 = document.getElementById('4');
-
 function testId4() {
     id4.classList.toggle('final');
-    if (id4.classList.contains('final')) {
-        qweq.innerText = 'Назад';
-    } else {
-        qwe.innerText = 'Подробнее о тарифе...'
-    }
 }
 
 var id5 = document.getElementById('5');
-
 function testId5() {
     id5.classList.toggle('final');
-    if (id5.classList.contains('final')) {
+}
+
+var id6 = document.getElementById('6');
+function testId6() {
+    id6.classList.toggle('final');
+}
+
+var id7 = document.getElementById('7');
+function testId7() {
+    id7.classList.toggle('final');
+}
+
+var id8 = document.getElementById('8');
+function testId8() {
+    id8.classList.toggle('final');
+}
+
+var id9 = document.getElementById('9');
+function testId9() {
+    id9.classList.toggle('final');
+}
+
+var id10 = document.getElementById('10');
+function testId10() {
+    id10.classList.toggle('final');
+}
+
+var id11 = document.getElementById('11');
+
+function testId11() {
+    id11.classList.toggle('final');
+    if (id11.classList.contains('final')) {
         qweq.innerText = 'Назад';
     } else {
         qwe.innerText = 'Подробнее о тарифе...'
     }
 }
 
-var id6 = document.getElementById('6');
+var id12 = document.getElementById('12');
+function testId12() {
+    id12.classList.toggle('final');
+}
 
-function testId6() {
-    id6.classList.toggle('final');
-    if (id6.classList.contains('final')) {
-        qweq.innerText = 'Назад';
-    } else {
-        qwe.innerText = 'Подробнее о тарифе...'
-    }
+var id13 = document.getElementById('13');
+function testId13() {
+    id13.classList.toggle('final');
+}
+
+var id14 = document.getElementById('14');
+function testId14() {
+    id14.classList.toggle('final');
 }
